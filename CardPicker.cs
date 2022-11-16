8f085f3838c63fbd340c840f84282af548c0eb12
Azizbek Olimov <aziz.olim3008@gmail.com>
2022-11-16 16:36:11 +0500

    Добавьте файлы проекта.

---
+++ CardPicker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickRandomCards
+{
+    internal class CardPicker
+    {
+        static Random random= new Random();
+
+        public string[] PickSomeCards(int _numberOfCards) 
+        {
+            string[] _pickedCards = new string[_numberOfCards];
+            for (int i = 0; i < _numberOfCards; i++) 
+            {
+                _pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+            return _pickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+            int value = random.Next(1,5);
+            if (value == 1) return "Spades";
+            if (value == 2) return "Hearts";
+            if(value == 3) return "Clubs";
+            return "Diamonds";
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            if (value == 1) return "Ace";
+            if (value == 11) return "Jack";
+            if (value == 12) return "Queen";
+            if (value == 13) return "King";
+            return value.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using PickRandomCards;
+
+internal class Program
+{
+    private static bool _play; 
+    private static void Main(string[] args)
+    {
+        
+        do
+        {
+            Console.Write("Enter the number of cards to pick: ");
+            string _line = Console.ReadLine();
+            if (int.TryParse(_line, out int _numberOfCards))
+            {
+                _play = false; 
+                CardPicker card = new CardPicker();
+                string[] cardArray = card.PickSomeCards(_numberOfCards);
+                for (int i = 0; i < cardArray.Length; i++)
+                {
+                    Console.WriteLine(cardArray[i]);
+
+                }    
+            }
+            else
+            {
+                Console.WriteLine("Please input right number");
+                _play = true;
+            }
+        }
+        while (_play == true);
+    }
+
+}
