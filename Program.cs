e73c829abdab9b944a76da1c026699c6a1e379e3
Azizbek Olimov <aziz.olim3008@gmail.com>
2022-11-17 15:34:06 +0500

    Refresh

--- Program.cs
+++ Program.cs
@@ -2,32 +2,39 @@
 
 internal class Program
 {
-    private static bool _play; 
+    private static bool _play;
+    private static bool _again;
     private static void Main(string[] args)
     {
-        
+
         do
         {
             Console.Write("Enter the number of cards to pick: ");
             string _line = Console.ReadLine();
-            if (int.TryParse(_line, out int _numberOfCards))
+            _play = !int.TryParse(_line, out int _numberOfCards);
+            if (!_play)
             {
-                _play = false; 
+
+
                 CardPicker card = new CardPicker();
                 string[] cardArray = card.PickSomeCards(_numberOfCards);
                 for (int i = 0; i < cardArray.Length; i++)
                 {
                     Console.WriteLine(cardArray[i]);
 
-                }    
+
+                }
+                Console.WriteLine("Play again?");
+                _again = Console.ReadLine() == "yes" ? true : false;
             }
             else
             {
                 Console.WriteLine("Please input right number");
-                _play = true;
+
             }
         }
-        while (_play == true);
-    }
+        while (_play || _again);
+    }     
+            
 
 }
